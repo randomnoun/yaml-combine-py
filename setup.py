8f737aabe2b769ce420ead9ae741057cf5b31af2
Greg Knox <knoxg@jacobistrategies.com>
2022-04-10 07:53:42 +1000

    description

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ setup(
     # This is a one-line description or tagline of what your project does. This
     # corresponds to the "Summary" metadata field:
     # https://packaging.python.org/specifications/core-metadata/#summary
-    description="A YAML pre-processor to combine one or more YAML files before swagger code generation.",
+    description="A YAML pre-processor to combine one or more YAML files with $xref references.",
     # This is an optional longer description of your project that represents
     # the body of text which users will see when they visit PyPI.
     #
