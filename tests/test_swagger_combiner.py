cd257f7e16223d0ae2baa2d830da5558142398d0
Greg Knox <knoxg@jacobistrategies.com>
2022-04-09 14:08:03 +1000

    unit tests, py3.6 output

--- tests/test_swagger_combiner.py
+++ tests/test_swagger_combiner.py
@@ -4,9 +4,10 @@
 
 import unittest
 import io
+import sys
+import logging
 from difflib import Differ
 from pprint import pprint
-
 
 from swagger_combine.swagger_combiner import SwaggerCombiner
 
@@ -14,28 +15,36 @@ from swagger_combine.swagger_combiner import SwaggerCombiner
 class TestSwaggerCombine(unittest.TestCase):
     def test_combine(self):
 
+        logging.basicConfig(level=logging.DEBUG)
         files = ["input.yaml"]
 
         sc = SwaggerCombiner()
         sc.set_files(files)
         sc.set_relative_dir("tests/data/t1/")
+        sc.set_verbose(False)
 
         output = io.StringIO()
         sc.combine(output)
         result = output.getvalue()
         output.close()
 
-        f = open("tests/data/t1/expected-output.yaml", "r", encoding="utf-8")
+        is_py37 = sys.version_info >= (3,7,0)
+        filename = "tests/data/t1/expected-output.yaml" if is_py37 else "tests/data/t1/expected-output-sorted.yaml" 
+        f = open(filename, "r", encoding="utf-8")
         expected_output = f.read()
         f.close()
+        self.nicer_assertEqual(result, expected_output)
 
+    def nicer_assertEqual(self, result, expected_output):
+        if (result==expected_output):
+            return # OK
+        
         print("\n=============\n")
         print("result is\n" + result)
 
         print("\n=============\n")
         print("expected is\n" + expected_output)
 
-        # self.assertEqual(result, expected_output)
         # nicer diff output
         d = Differ()
         result = list(
@@ -45,6 +54,7 @@ class TestSwaggerCombine(unittest.TestCase):
             )
         )
         pprint(result)
+        self.assertEqual(result, expected_output)
 
 
 if __name__ == "__main__":
